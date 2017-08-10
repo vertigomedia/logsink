bf6ed03870e4a0aa42b60b69c2c5860ee81c2272
aaltergot <aaltergot@issart.com>
2017-08-10 17:57:30 +0600

    timestamp format; locations

--- src/System/Logging/LogSink/Internal.hs
+++ src/System/Logging/LogSink/Internal.hs
@@ -6,7 +6,9 @@ import           System.Logging.LogSink.Compat
 
 import           Control.Concurrent
 import           Data.Char
+import           Data.Maybe (fromMaybe)
 import           Data.Time.Clock
+import           Data.Time.Format (formatTime, defaultTimeLocale)
 import           Data.Time.LocalTime ()
 import           System.Logging.Facade.Types
 
@@ -15,7 +17,15 @@ type Format = LogRecord -> IO String
 defaultFormatString :: String
 defaultFormatString = "{level}: {message}"
 
-data Node = Level | Message | Timestamp | ThreadId | Literal String
+data Node = Level
+          | Message
+          | Timestamp
+          | ThreadId
+          | Literal String
+          | LocationPackage
+          | LocationModule
+          | LocationFile
+          | LocationLine
   deriving (Eq, Show)
 
 formatNodes :: [Node] -> LogRecord -> IO String
@@ -25,9 +35,22 @@ formatNodes nodes LogRecord{..} = concat <$> mapM evalNode nodes
     evalNode node = case node of
       Level -> return (show logRecordLevel)
       Message -> return logRecordMessage
-      Timestamp -> show <$> getCurrentTime
-      ThreadId -> show <$> myThreadId
+      Timestamp -> formatISO8601Millis <$> getCurrentTime
+      ThreadId -> do
+        tid <- myThreadId
+        return $ filter isNumber (show tid)
       Literal s -> return s
+      LocationPackage -> return $ fromMaybe "" $ locationPackage <$> logRecordLocation
+      LocationModule -> return $ fromMaybe "" $ locationModule <$> logRecordLocation
+      LocationFile -> return $ fromMaybe "" $ locationFile <$> logRecordLocation
+      LocationLine -> return $ fromMaybe "" $ show . locationLine <$> logRecordLocation
+
+formatISO8601Millis :: UTCTime -> String
+formatISO8601Millis t =
+    let str = formatTime defaultTimeLocale "%FT%T%Q" t in
+    -- Padding is needed because `formatTime` with "%Q" does not create trailing zeros
+    let strPadded = str ++ if length str == 19 then ".000" else "000" in
+    take 23 strPadded ++ "Z"
 
 parseNodes :: String -> Either String [Node]
 parseNodes = fmap (filter $ not . isEmpty) . go ""
@@ -36,7 +59,16 @@ parseNodes = fmap (filter $ not . isEmpty) . go ""
     isIdChar c = isAlphaNum c || (c `elem` "-_")
 
     lookupNode :: String -> Maybe Node
-    lookupNode key = lookup key [("level", Level), ("message", Message), ("timestamp", Timestamp), ("thread-id", ThreadId)]
+    lookupNode key = lookup key
+      [ ("level", Level)
+      , ("message", Message)
+      , ("timestamp", Timestamp)
+      , ("thread-id", ThreadId)
+      , ("location-package", LocationPackage)
+      , ("location-module", LocationModule)
+      , ("location-file", LocationFile)
+      , ("location-line", LocationLine)
+      ]
 
     go :: String -> String -> Either String [Node]
     go acc input = case input of
@@ -49,7 +81,7 @@ parseNodes = fmap (filter $ not . isEmpty) . go ""
       x:xs -> go (x:acc) xs
 
     lit :: String -> Node
-    lit acc = (Literal . reverse) acc
+    lit = Literal . reverse
 
     isEmpty :: Node -> Bool
     isEmpty (Literal "") = True
